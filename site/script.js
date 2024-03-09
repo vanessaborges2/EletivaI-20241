61ffb286139f77f25d6502c4c8e493e591b8dcea
vanessaborges2 <vanessa.borges2@fatec.sp.gov.br>
2024-03-08 22:09:45 -0300

    Update script.js

---
+++ site/script.js
@@ -0,0 +1,17 @@
+let listras = document.querySelector("#listras");
+let menu = document.querySelector("#menu");
+let itens = document.querySelectorAll(".item");
+
+function abrirMenu(){
+    if (menu.classList.contains("active")){
+        menu.classList.remove("active");
+        listras.querySelector("a").innerHtml = 
+            "<i class='fas fa-bars'></i>";
+    } else{
+        menu.classList.add("active");
+        listras.querySelector("a").innerHtml = 
+            "<i class='fas fa-times'></i>";
+    }
+}
+
+listras.addEventListener("click", abrirMenu);
